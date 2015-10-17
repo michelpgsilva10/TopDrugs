6582344fc4d7a9cb61998e4f67d0e00c90ad0a69
Roberto <Roberto@Roberto-pc>
2015-10-17 17:47:05 -0400

    Fornecedor

--- TopDrugs1/src/java/vo/FornecedorVO.java
+++ TopDrugs1/src/java/vo/FornecedorVO.java
@@ -1,15 +1,54 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+CREATE TABLE FORNECEDOR
+(
+cod_fornecedor NUMBER,
+cnpj VARCHAR(15),
+razao_social VARCHAR(100),
+nome_fantasia VARCHAR(100),
+email VARCHAR(100),
+cidade VARCHAR(100),
+uf CHAR(2),
+CONSTRAINT pk_fornecedor_005 PRIMARY KEY (cod_fornecedor)
+)
+
+ */
 package vo;
 
-/**
- *
- * @author aluno
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="fornecedor")
 public class FornecedorVO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="cod_fornecedor")
+    private int codigoGerente;
+    
+    @Column(name="cnpj")
+    private String cnpj;
+    
+    @Column(name="razao_social")
+    private String razaoSocial;
+     
+    @Column(name="nome_fantasia")
+    private String nomeFantasia;
+     
+    @Column(name="email")
+    private String email;
+     
+    @Column(name="cidade")
+    private String cidade;
+      
+    @Column(name="uf")
+    private String uf;
+     
     
 }
+
