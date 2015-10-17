931c9922bfc97cc354aa94bec7590830ca651ad1
Roberto <Roberto@Roberto-pc>
2015-10-17 17:26:38 -0400

    PessoaVO montado

--- TopDrugs1/src/java/vo/PessoaVO.java
+++ TopDrugs1/src/java/vo/PessoaVO.java
@@ -6,10 +6,177 @@
 
 package vo;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.eclipse.persistence.annotations.CloneCopyPolicy;
+
 /**
  *
  * @author aluno
  */
+/*
+cod_pessoa NUMBER, 
+cpf  VARCHAR(11), 
+nome VARCHAR(255), 
+email VARCHAR(50), 
+data_nascimento DATE, 
+bairro VARCHAR(100), 
+rua VARCHAR(255), 
+numero NUMBER(4) 
+complemento VARCHAR(50), 
+numero VARCHAR (10),
+cidade VARCHAR(100),
+uf CHAR(2)
+cep VARCHAR(9),
+local_de_trabalho VARCHAR(150),
+data_cadastro DATE,
+*/
+@Entity
+@Table(name="pessoa")
 public class PessoaVO {
+
+    public PessoaVO() {
+    }
+ 
+    
+ @Id
+ @GeneratedValue(strategy = GenerationType.SEQUENCE)
+ @Column(name="cod_pessoa")
+ private int codigoPessoa;
+ @Column(name="nome")
+ private String nome;
+ @Column(name="cpf")
+ private String cpf;
+ @Column(name="email")
+ private String email;
+ @Column(name="data_nascimento")
+ private Date dataNascimento;
+ @Column(name="bairro")
+ private String bairro;
+ @Column(name="rua")
+ private String rua;
+ @Column(name="numero")
+ private int numero;
+ @Column(name="complemento")
+ private String complemento;
+ @Column(name="cidade")
+ private String cidade;
+ @Column(name="uf")
+ private String uf;
+ @Column(name="local_de_trabalho")
+ private String localDeTrabalho;
+ @Column(name="data_cadastro")
+ private String dataCadastro;
+
+    public int getCodigoPessoa() {
+        return codigoPessoa;
+    }
+
+    public void setCodigoPessoa(int codigoPessoa) {
+        this.codigoPessoa = codigoPessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getLocalDeTrabalho() {
+        return localDeTrabalho;
+    }
+
+    public void setLocalDeTrabalho(String localDeTrabalho) {
+        this.localDeTrabalho = localDeTrabalho;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+ 
+    
     
 }
